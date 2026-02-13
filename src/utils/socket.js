8188d6cad37e1f0abf214b914d833c9593c4137f
Darjan <darjan.developer@gmail.com>
2026-02-13 11:30:41 +0100

    -- improve socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,9 +4,12 @@ const SOCKET_URL = import.meta.env.VITE_API_URL
   || 'http://localhost:5000';   // match your backend PORT
 
 export const socket = io(SOCKET_URL, {
-  withCredentials: true,        // important if you use cookies / auth
-  autoConnect: false,           // we connect manually after authorization
+ withCredentials: true,
+  autoConnect: false,
+  transports: ["polling", "websocket"],
+
   reconnection: true,
-  reconnectionAttempts: 5,
+  reconnectionAttempts: Infinity,
   reconnectionDelay: 1000,
+  reconnectionDelayMax: 5000,
 });
