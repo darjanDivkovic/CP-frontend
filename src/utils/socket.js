b1aafe5cab46233da69c14b49c194f5e39ef119f
Darjan <darjan.developer@gmail.com>
2026-02-13 11:37:52 +0100

    -- update socket url

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = import.meta.env.VITE_API_URL 
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL 
   || 'http://localhost:5000';   // match your backend PORT
 
 export const socket = io(SOCKET_URL, {
